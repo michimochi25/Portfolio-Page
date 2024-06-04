b67fc52a4a00e60fe5e2234d74e3fb8aa33e300f
Giselle <cryponiess@gmail.com>
2024-06-04 13:57:42 +1000

    setup

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-[#1E1E24] h-screen text-[#FFF8F0]">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
