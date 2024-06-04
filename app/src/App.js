93a5a0a63cbacac5d84b9192f902c4528f4465a8
Giselle <cryponiess@gmail.com>
2024-06-04 13:50:34 +1000

    hard reset bcs idk wtf happened

--- app/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="h-screen bg-[#1E1E24] text-center p-10">
-      <p className="text-[#FFF8F0] text-5xl font-bold">This is a Landing Page</p>
-    </div>
-  );
-}
-
-export default App;
